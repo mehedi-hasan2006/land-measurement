6abf61acd0211d899aec96b80e7b8c187628f2a9
mehedi-hasan2006 <mehedihasan811914@gmail.com>
2026-02-19 18:43:17 +0600

    done

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ document.getElementById('calculate-btn-1').addEventListener('click', function(){
     let north = getValueFromInput('north');
     let south = getValueFromInput('south');
 
+    if(east===0 || west===0 || north===0 || south===0){
+        alert("সবগুলো ঘর পূরণ করুন। ")
+        return
+    }
+
     let avg1= ((east+west)/2);
     let avg2 = ((north+south)/2)
 
-    let totalLand = ((avg1 * avg2) / 435.6).toFixed(4);
+    let totalLand = ((avg1 * avg2) / 435.6);
 
     let result = document.getElementById('result');
-    result.innerText = totalLand;
+    result.innerText = `${totalLand} শতাংশ`;
 
     let resultContainer = document.getElementById('result-container');
     resultContainer.classList.remove("hidden");
