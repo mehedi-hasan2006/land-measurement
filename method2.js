7e09ebb8a27860e1689e02f5be9e77fc104dfee2
mehedi-hasan2006 <mehedihasan811914@gmail.com>
2026-02-19 22:01:50 +0600

    fixed calculation problem by trigonometric method

--- method2.js
+++ method2.js
@@ -8,26 +8,31 @@ document
 
     if (east1 === 0 || west1 === 0 || north1 === 0 || south1 === 0) {
       alert("সবগুলো ঘর পূরণ করুন। ");
-      //   return;
+        return;
     }
 
-    let hyperbola1 = Math.sqrt((east1 * east1) + (south1 * south1));
-    let s1 =  (east1 + south1 + hyperbola1) /2;
-    let area1 = Math.sqrt(s1 * (s1 -  east1) * (s1 - south1) * (s1 - hyperbola1));
+    let hyperbola1 = Math.sqrt(east1 * east1 + south1 * south1);
+    let s1 = (east1 + south1 + hyperbola1) / 2;
+    let area1 = Math.sqrt(
+      s1 * (s1 - east1) * (s1 - south1) * (s1 - hyperbola1),
+    );
 
-    let hyperbola2 = Math.sqrt((west1 * west1) + (north1 * north1));
-    let s2= (west1 + north1 + hyperbola2);
-    let area2 = Math.sqrt(s2 * (s2-west1) * (s2 - north1) * (s2 - hyperbola2));
+    
+    let hyperbola2 = Math.sqrt(west1 * west1 + north1 * north1);
+    let s2 = (west1 + north1 + hyperbola2)/2;
+    let area2 = Math.sqrt(
+      s2 * (s2 - west1) * (s2 - north1) * (s2 - hyperbola2),
+    );
+
+
 
     const totalArea = area1 + area2;
     const totalLand = totalArea / 435.6;
 
 
-    let result1 = document.getElementById('result1');
+    let result1 = document.getElementById("result1");
     result1.innerText = `${totalLand} শতাংশ`;
 
-    let resultContainer = document.getElementById('result-container1');
+    let resultContainer = document.getElementById("result-container1");
     resultContainer.classList.remove("hidden");
-
-  
   });
