9db299ad9ed4386e531689e3a0c39ca35eb63b80
mehedi-hasan2006 <mehedihasan811914@gmail.com>
2026-02-20 19:21:15 +0600

    license file add & some issued fixed

--- method3.js
+++ method3.js
@@ -6,12 +6,12 @@ document
     let c = getValueFromInput("thirdValue");
 
     let s = (a + b + c) / 2;
-    let area = Number(Math.sqrt(s * (s - a) * (s - b) * (s - c)));
+    let area = Number(Math.sqrt(s * (s - a) * (s - b) * (s - c))).toFixed(3);
 
     console.log(a, b, c, s, area);
 
     let result2 = document.getElementById("result2");
-    result2.innerText = `${area}`;
+    result2.innerText = `${area} বর্গ একক`;
 
     let resultContainer = document.getElementById("result-container2");
     resultContainer.classList.remove("hidden");
